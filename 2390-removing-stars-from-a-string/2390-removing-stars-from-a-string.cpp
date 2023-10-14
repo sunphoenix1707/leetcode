94077544ed27f1d2e924b5cba46c7334f852f7ba
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2023-10-14 10:59:15 +0530

    Time: 87 ms (66.72%), Space: 27.4 MB (22.94%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++) {
+            if(s[i]=='*') {
+                st.pop();
+            }
+            else st.push(s[i]);
+        }
+        string ans;
+        while(st.size()>=1) {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
