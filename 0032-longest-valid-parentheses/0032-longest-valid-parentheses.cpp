12e2bc49b73ec28dc5a16cf8e1a1f92e0cd0a377
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2023-10-18 07:57:42 +0530

    Time: 7 ms (19.60%), Space: 7.8 MB (13.13%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>st;
+        st.push(-1);
+        int i=0;
+        int n=s.size();
+        int maxi=0;
+        while(i<n) {
+            if(s[i]=='(') {
+                st.push(i);
+                
+            }
+           else {
+               st.pop();
+                if(st.empty()) {
+                st.push(i);
+                     
+            }
+            else {
+                maxi=max(maxi,(i-st.top()));
+            }
+               
+                  
+               
+           }
+           
+             i++; 
+        }
+        return maxi;
+    }
+//     int longestValidParentheses(string s) {
+//     stack<int> st;
+//     st.push(-1);
+//     int n = s.size();
+//     int maxi = 0;
+//     for (int i = 0; i < n; i++) {
+//         if (s[i] == '(') {
+//             st.push(i);
+//         } else {
+//             st.pop();
+//             if (st.empty()) {
+//                 st.push(i);
+//             } else {
+//                 maxi = max(maxi, i - st.top());
+//             }
+//         }
+//     }
+//     return maxi;
+// }
+};
