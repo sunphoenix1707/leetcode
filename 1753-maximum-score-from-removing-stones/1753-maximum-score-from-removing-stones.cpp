cb39afd03d5ce25761635ab1f0cb6dcffe119dc3
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2023-11-05 10:50:15 +0530

    Time: 87 ms (43.18%), Space: 6.4 MB (45.63%) - LeetHub

---
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+       priority_queue<int> pq;
+        pq.push(a); pq.push(b); pq.push(c);
+        int ans=0;
+        for(;pq.size()>1;ans++) {
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+           if(x-1)  pq.push(x-1);
+            if(y-1) pq.push(y-1);
+        }
+        return ans;
+    }
+};
