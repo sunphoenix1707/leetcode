4d71812d1c84124a920d50a1c1f4de94b9034311
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2023-11-18 10:59:01 +0530

    Time: 83 ms (22.58%), Space: 17.7 MB (55.36%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int minDeletions(string s) {
-    unordered_map<char,int>mp;
+        unordered_map<char,int>mp;
         for(auto c:s) {
             mp[c]++;
         }
-        vector<int> freq;
-        unordered_set<int> set; 
-        int ans=0;
-        for(auto p: mp) {
-            int temp=p.second;
-            while(set.find(temp)!=set.end() && temp!=0) {
-                ans++;
-                temp--;
-            }
-           set.insert(temp);
+        vector<int> a;
+        for(auto p:mp) {
+            a.push_back(p.second);
         }
-       return ans;
+        int ans=0;
+        sort(a.begin(),a.end(),greater<int>());
+        for(int i=1;i<a.size();i++) {
+                if(a[i]>=a[i-1]) {
+                    int temp=a[i];
+                    a[i]=a[i-1] ? a[i-1]-1:0;
+                       ans+=temp-a[i];
+                }
+           
+        }
+         return ans;
     }
 };
-//
-// "bbcebab" temp!=0 condition we are using why
-// b=4
-//     c=1
-//       e=1  
-//          a=1
-    
+// suppose freq 3,3 ,2 in decreasing order
+// v[1]  > = v[0] 3 >= 3
+// temp=v[1]=3
+//3=3 ? 3-1:0
+//ans+=3-2=1 
+//now 3 2 2 i is at last index
+// again ans=2
