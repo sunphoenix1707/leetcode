d2a5af65c26754bc18c1882c682ef953468fee80
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2023-11-18 13:02:33 +0530

    Time: 521 ms (17.60%), Space: 170.4 MB (14.05%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
         vector<int> p(n);
-        for(auto r:roads) {
-            p[r[0]]++,p[r[1]]++;
+        for(auto e:roads) {
+            p[e[0]]++,p[e[1]]++;
         }
-        sort(p.begin(),p.end());
+        
+        vector<pair<int , int >> v;
+        for(int i=0 ;i<p.size() ;i++) {
+            v.push_back({p[i],i});
+        }
         long long ans=0;
-        for(int i=0;i<p.size();i++) {
-            ans+=(long long) p[i]*(i+1);
-        }
+        sort(v.begin(),v.end());
+        unordered_map<int ,int >rank;
+        for(int i=0; i<v.size();i++)
+            rank[v[i].second] = i+1;
+        for(auto e: roads) 
+            ans+=rank[e[0]] + rank[e[1]];
         return ans;
     }
 };
