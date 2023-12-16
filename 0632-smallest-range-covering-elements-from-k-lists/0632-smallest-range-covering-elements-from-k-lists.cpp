078d35f067ea24f29bea633c24a7d374a36666d9
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2023-12-16 07:14:40 +0530

    Time: 63 ms (26.48%), Space: 15.9 MB (80.62%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
+    using arr= array<int,3> ;
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        unordered_map<int,int>mp;
-        vector<pair<int,int>> v;
-        for(int i=0;i<nums.size();i++) {
-            for(int j=0 ; j<nums[i].size() ;j++)
-            
-                v.push_back({nums[i][j],i});
-            
+       priority_queue< arr,vector<arr> , greater<arr>> pq;
+        int maxi= INT_MIN;
+        for(int i=0 ; i<nums.size() ; i++)
+        maxi= max(maxi , nums[i][0]) , pq.push({nums[i][0] , 0 ,i});
+        int lo = -1e5, hi= 1e5;
+        while(pq.size() == nums.size())
+        {
+            auto [mini, i ,j] = pq.top();  pq.pop();
+            if(maxi-mini < hi - lo) lo=mini , hi=maxi;
+            else if(maxi - mini == hi-lo && mini < lo)  lo=mini , hi=maxi;
+            if(i+1 < nums[j].size())
+                maxi = max(maxi , nums[j][i+1]) , pq.push({nums[j][i+1],i+1 ,j});
         }
-        sort(v.begin(),v.end());
-        int i=0, j=0;
-        vector<int> ans{-100000, 100000};
-        while(j < v.size()) {
-            mp[v[j].second]++;
-            while(mp.size()==nums.size()) {
-            if(v[j].first - v[i].first < ans[1]-ans[0]) ans={v[i].first, v[j].first};
-            mp[v[i].second]--;
-                if(!mp[v[i].second])  mp.erase(v[i].second);
-                  i++;
-                }
-            j++;
-        }
-        return ans;
+        return {lo, hi};
     }
 };
