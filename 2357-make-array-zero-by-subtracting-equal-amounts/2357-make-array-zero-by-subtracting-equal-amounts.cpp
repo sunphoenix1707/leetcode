b82061d69d25de0a8cfc6e0dc4f979ea502e4e9b
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2023-12-21 05:38:16 +0530

    Time: 2 ms (51.73%), Space: 8.8 MB (13.82%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    //time complexity-0(n2)
+   
     int minimumOperations(vector<int>& nums) {
-       sort(nums.begin(),nums.end()); 
-        int a = 0 , c = 0 , j;
-        for(int i=0;i<nums.size();i++) {
-            if(nums[i]>0)  {
-                
-                a = nums[i];
-                c++;
-            for( j=i; j<nums.size(); j++)
-                nums[j]-=a;
-              }
+
+        map<int,int> mp;
+        for(auto i: nums)
+        {
+           if(i!=0) mp[i]++;
         }
-       return c;
-       
+       return mp.size();
     }
 };
