acaf581f435c9340fb58834d122b2b5906db26d1
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2023-12-24 15:56:33 +0530

    Time: 69 ms (89.26%), Space: 51.8 MB (13.38%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int maxi=INT_MIN;
+        for(int i=0 ; i<arr.size() ; i++) 
+        {
+            if(i==0) arr[0]=1;
+            else if(abs(arr[i]-arr[i-1])>1) {
+                arr[i]=arr[i-1]+1;
+               
+            }
+             maxi=max(maxi,arr[i]);
+        }
+        return maxi;
+    }
+};
