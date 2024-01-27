48a4209afd2189afa44312ee3cb3a7e1f02cd8e2
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2024-01-28 03:28:31 +0530

    Time: 71 ms (86.63%), Space: 60.3 MB (64.95%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -12,33 +12,52 @@
 class Solution {
     //level order traversal from bottom and adding sum
 public:
+//     int deepestLeavesSum(TreeNode* root) {
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         int sum=0;
+//         vector<vector<int>> ans;
+//         while(!q.empty()) {
+//             int n=q.size();
+//             vector<int> v;
+//             while(n--) {
+//                 TreeNode* temp=q.front();
+//                 q.pop();
+//                 v.push_back(temp->val);
+//                 if(temp->left)
+//                     q.push(temp->left);
+//                 if(temp->right)
+//                     q.push(temp->right);
+                
+//             }
+//             ans.push_back(v);
+//         }
+//         reverse(ans.begin(),ans.end());
+//         for(int i=0; i<ans.size();i++) {
+//             for(int j=0;j<ans[i].size();j++) {
+//                 sum+=ans[i][j];
+//             }
+//             break;
+//         }
+//         return sum;
+//     }
     int deepestLeavesSum(TreeNode* root) {
+        int ans = 0;
         queue<TreeNode*> q;
         q.push(root);
-        int sum=0;
-        vector<vector<int>> ans;
+        
         while(!q.empty()) {
-            int n=q.size();
-            vector<int> v;
+            int n = q.size();
+            ans = 0;
+            
             while(n--) {
-                TreeNode* temp=q.front();
-                q.pop();
-                v.push_back(temp->val);
-                if(temp->left)
-                    q.push(temp->left);
-                if(temp->right)
-                    q.push(temp->right);
+                TreeNode* temp = q.front(); q.pop();
+                ans += temp -> val;
                 
+                if(temp -> left) q.push(temp -> left);
+                if(temp -> right) q.push(temp -> right);
             }
-            ans.push_back(v);
         }
-        reverse(ans.begin(),ans.end());
-        for(int i=0; i<ans.size();i++) {
-            for(int j=0;j<ans[i].size();j++) {
-                sum+=ans[i][j];
-            }
-            break;
-        }
-        return sum;
+        return ans;
     }
 };
