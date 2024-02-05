0b4ebbcb43429561720b51d5fc121e6c58091224
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2024-02-05 19:49:50 +0530

    Time: 1204 ms (7.53%), Space: 186.6 MB (12.75%) - LeetHub

--- 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.cpp
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.cpp
@@ -1,74 +1,56 @@
-using ull = unsigned long long;
 class Solution {
-private:
-    TreeNode* buildTree(vector<int>& parents, vector<int>& nodes) {
-        int n = parents.size();
-        unordered_map<int,TreeNode*> mp;
-        for (int i = 0; i<n; ++i) 
-            mp[i] = new TreeNode(nodes[i]);
-    
-        TreeNode* root = NULL;
-        root = mp[0];
-        
-        for (int i = 1; i < parents.size(); ++i) {
-            if (mp[parents[i]]->left == NULL) 
-                mp[parents[i]]->left = mp[i];
-            else 
-                mp[parents[i]]->right = mp[i];
-        }
-
-        return root;
-    }
-    
-    unordered_map<ull,int> mp2; // {score,freq}
-    ull dfs(TreeNode* root, ull &maxi, int n){
-        if(root == NULL){
-            return 0;
-        }
-        
-        ull leftCnt = dfs(root->left,maxi,n);
-        ull rightCnt = dfs(root->right,maxi,n);
-        
-        ull upperCnt = n - (leftCnt + rightCnt + 1);
-        
-        ull val = 1 * (leftCnt > 0 ? leftCnt : 1) * (rightCnt > 0 ? rightCnt : 1) * (upperCnt > 0 ? upperCnt : 1);
-        
-        mp2[val]++;
-        
-        // cout<<root->val<<" -> ";
-        // cout<<leftCnt<<" "<<rightCnt<<" and val = "<<val<<endl;
-        
-        maxi = max(maxi, leftCnt+rightCnt+1);
-    
-        return leftCnt+rightCnt+1;
-    }
-
 public:
+    using ull = unsigned long long;
+    TreeNode* BuildTree(vector<int> &parents, vector<int> &nodes)
+    {     int n=parents.size();
+        unordered_map<int,TreeNode*>mp;
+        for(int i=0; i<n;++i)
+            mp[i]= new TreeNode(nodes[i]);
+        TreeNode* root=NULL;
+        root=mp[0];
+        for(int i=1;i<parents.size();++i)
+        {
+            if(mp[parents[i]]->left==NULL)
+                mp[parents[i]]->left=mp[i];
+                else 
+                    mp[parents[i]]->right=mp[i];
+        }
+        return  root;
+    }
+    unordered_map<ull,int>mp2;
+    ull dfs(TreeNode* root,ull &maxi,int n) {
+        if(root==NULL) return 0;
+        ull leftc=dfs(root->left,maxi,n);
+        ull rightc=dfs(root->right,maxi,n);
+        ull upper= n - (leftc+rightc+1);
+        
+        ull val = 1*(leftc>0?leftc:1)*(rightc>0?rightc:1)*(upper>0?upper:1);
+        mp2[val]++;
+        // cout<<root->val<<" -> ";
+        // cout<<leftc<<" "<<rightc<<" and val = "<<val<<endl;
+        maxi=max(maxi,leftc+rightc+1);
+        return leftc+rightc+1;
+        
+    }
     int countHighestScoreNodes(vector<int>& parents) {
         int n = parents.size();
         vector<int> nodes(n);
-        
-        for(int i = 0; i<n; i++) 
-            nodes[i] = i;
-        
-        TreeNode* root = buildTree(parents,nodes);
-        
-        ull maxi = 0;
-        ull num = dfs(root, maxi, n); 
-        // cout<<endl;
-        
-        ull maxScore = 0;
-        for(auto it : mp2) 
-            maxScore = max(maxScore,it.first);
-        
-        int res = 0;
-        for(auto it : mp2){
-            if(it.first == maxScore){
-                res = it.second;
+        for(int i=0;i<n;++i)
+            nodes[i]=i;
+        TreeNode* root = BuildTree(parents,nodes);
+        ull maxi=0;
+        ull num=dfs(root,maxi,n);
+            ull maxscore=0;
+           for(auto it:mp2) 
+               maxscore=max(maxscore,it.first);
+           
+           int res=0;
+            for(auto it:mp2) {
+                  if(it.first==maxscore) {
+                    res=it.second;
                 break;
+                  }
             }
-        }
-        
         return res;
     }
 };
