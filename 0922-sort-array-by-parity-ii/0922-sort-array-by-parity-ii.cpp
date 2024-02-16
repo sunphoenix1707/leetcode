d0f2bb9987142947ae430320d68da6d4d98aea4e
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2024-02-16 08:28:21 +0530

    Time: 17 ms (44.23%), Space: 23.7 MB (64.02%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int pos=0;
+        int neg=1;
+        vector<int> ans(nums.size(),0);
+        for(int i=0; i<nums.size();i++)
+        {
+            if(nums[i] & 1) {
+                ans[neg]=nums[i];
+                neg+=2;
+            }
+            else {
+                ans[pos]=nums[i];
+                pos+=2;
+            }
+        }
+        return ans;
+    }
+};
