fe1472772879d814e248e7608aab0539329e9c5b
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2024-02-17 12:47:09 +0530

    Time: 34 ms (70.40%), Space: 38.4 MB (39.74%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    bool canReach(vector<int>& arr, int start) {
-        queue<int>  q;
-        q.push(start);
-        int n = arr.size();
-        vector<int> vis(n);
-        vis[start]=1;
-         while(!q.empty()) {
-             int t = q.front();
-             q.pop();
-             if(!arr[t]) return true; 
-             if(t+arr[t] < n && !vis[t+arr[t]]) {
-                 q.push(t+arr[t]);
-                 vis[t + arr[t]]=1;
-             }
-              if(t-arr[t] >=0  && !vis[t-arr[t]]) {
-                 q.push(t-arr[t]);
-                 vis[t - arr[t]]=1;
-             }
-         }
-        
+  bool help(vector<int> &arr, int start, vector<int> &vis, int n) {
+    if (start < 0 || start >= n || vis[start]) {
         return false;
     }
+
+    vis[start] = true;
+
+    if (arr[start] == 0) {
+        return true;
+    }
+
+    bool a = false, b = false;
+
+    if (start + arr[start] < n) {
+        a = help(arr, start + arr[start], vis, n);
+    }
+
+    if (start - arr[start] >= 0) {
+        b = help(arr, start - arr[start], vis, n);
+    }
+
+    return a || b;
+}
+
+bool canReach(vector<int> &arr, int start) {
+    vector<int> vis(arr.size(), 0);
+    int n = arr.size();
+    return help(arr, start, vis, n);
+}
 };
